effd87139b7f0f7e2fd3e4f979f8a50c19f3969e
Siraj <sirajnwings@gmail.com>
2016-07-19 15:37:15 +0530

    import

--- app/src/main/java/com/example/nseipl25/myapplicationwithlollipop/MainActivity.java
+++ app/src/main/java/com/example/nseipl25/myapplicationwithlollipop/MainActivity.java
@@ -10,7 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-//import android.util.Log;
+import android.util.Log;
 //import android.view.Menu;
 //import android.view.MenuItem;
 //import android.view.MotionEvent;
